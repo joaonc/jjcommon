498ec4e3ae100f8ade442546fc43d1d883845e2c
joaonc@gmail.com <joaonc@gmail.com@5157063a-5bbc-a59b-71ea-1f48db5e8bc2>
2012-01-10 13:43:58 +0000

    Adding JUnit testing and more declarations in the pom file for spring, hibernate and other utils.

---
+++ trunk/src/main/java/com/jjcommon/StringUtil.java
@@ -0,0 +1,10 @@
+package com.jjcommon;
+
+/**
+ * Utility class for strings.
+ */
+public class StringUtil {
+    public static boolean isNullOrEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+}
