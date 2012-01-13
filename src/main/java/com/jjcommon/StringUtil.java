5d80b8e2aa76caaf3c9de7fbb0765d8b5e47c5e9
joaonc <joaonc@gmail.com>
2012-01-13 01:04:13 +0000

    Adding method in StringUtil as a stub for now. Implementation will come later.

--- src/main/java/com/jjcommon/StringUtil.java
+++ src/main/java/com/jjcommon/StringUtil.java
@@ -7,4 +7,13 @@ public class StringUtil {
     public static boolean isNullOrEmpty(String string) {
         return string == null || string.isEmpty();
     }
+
+    /**
+     * Creates a random string.
+     * @param nrChars The number of characters the string will have.
+     * @return A random string.
+     */
+    public static String createRandom(int nrChars) {
+        return "ABC";  // TODO: generate randcom.
+    }
 }
